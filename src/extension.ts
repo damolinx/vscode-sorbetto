2d2d730f662b74c0617610c5a7e83f46ba7f1f1e
Daniel Molina <damolina@outlook.com>
2025-03-29 12:19:30 -0700

    Structure update

--- src/extension.ts
+++ src/extension.ts
@@ -19,13 +19,13 @@ import { ServerStatus, RestartReason } from './types';
  * Extension entrypoint.
  */
 export async function activate(context: ExtensionContext) {
-  const sorbetExtensionContext = new SorbetExtensionContext(context);
+  const extensionContext = new SorbetExtensionContext(context);
 
   context.subscriptions.push(
-    sorbetExtensionContext,
-    sorbetExtensionContext.configuration.onLspConfigChange(
+    extensionContext,
+    extensionContext.configuration.onLspConfigChange(
       async ({ oldLspConfig, newLspConfig }) => {
-        const { statusProvider } = sorbetExtensionContext;
+        const { statusProvider } = extensionContext;
         if (oldLspConfig && newLspConfig) {
           // Something about the config changed, so restart
           await statusProvider.restartSorbet(RestartReason.CONFIG_CHANGE);
@@ -38,63 +38,40 @@ export async function activate(context: ExtensionContext) {
     ),
   );
 
-  const statusBarEntry = new SorbetStatusBarEntry(sorbetExtensionContext);
+  const statusBarEntry = new SorbetStatusBarEntry(extensionContext);
   context.subscriptions.push(statusBarEntry);
 
   // Register providers
   context.subscriptions.push(
     workspace.registerTextDocumentContentProvider(
       SORBET_SCHEME,
-      new SorbetContentProvider(sorbetExtensionContext),
+      new SorbetContentProvider(extensionContext),
     ),
   );
 
   // Register commands
+  const r = commands.registerCommand;
   context.subscriptions.push(
-    commands.registerCommand(cmdIds.COPY_SYMBOL_COMMAND_ID, () =>
-      copySymbolToClipboard(sorbetExtensionContext),
+    r(cmdIds.COPY_SYMBOL_COMMAND_ID, () => copySymbolToClipboard(extensionContext)),
+    r(cmdIds.SHOW_ACTIONS_COMMAND_ID, () => showSorbetActions(extensionContext)),
+    r(cmdIds.SHOW_CONFIG_PICKER_COMMAND_ID, () => showSorbetConfigurationPicker(extensionContext)),
+    r(cmdIds.SHOW_OUTPUT_COMMAND_ID, () => extensionContext.logOutputChannel.show(true)),
+    r(cmdIds.SORBET_ENABLE_COMMAND_ID, () => extensionContext.configuration.setEnabled(true)),
+    r(cmdIds.SORBET_DISABLE_COMMAND_ID, () => extensionContext.configuration.setEnabled(false)),
+    r(cmdIds.SORBET_RESTART_COMMAND_ID, (reason: RestartReason = RestartReason.COMMAND) =>
+        extensionContext.statusProvider.restartSorbet(reason),
     ),
-    commands.registerCommand(cmdIds.SHOW_ACTIONS_COMMAND_ID, () =>
-      showSorbetActions(sorbetExtensionContext),
-    ),
-    commands.registerCommand(cmdIds.SHOW_CONFIG_PICKER_COMMAND_ID, () =>
-      showSorbetConfigurationPicker(sorbetExtensionContext),
-    ),
-    commands.registerCommand(cmdIds.SHOW_OUTPUT_COMMAND_ID, () =>
-      sorbetExtensionContext.logOutputChannel.show(true),
-    ),
-    commands.registerCommand(cmdIds.SORBET_ENABLE_COMMAND_ID, () =>
-      sorbetExtensionContext.configuration.setEnabled(true),
-    ),
-    commands.registerCommand(cmdIds.SORBET_DISABLE_COMMAND_ID, () =>
-      sorbetExtensionContext.configuration.setEnabled(false),
-    ),
-    commands.registerCommand(
-      cmdIds.SORBET_RESTART_COMMAND_ID,
-      (reason: RestartReason = RestartReason.COMMAND) =>
-        sorbetExtensionContext.statusProvider.restartSorbet(reason),
-    ),
-    commands.registerCommand(cmdIds.SORBET_SAVE_PACKAGE_FILES, () =>
-      savePackageFiles(sorbetExtensionContext),
-    ),
-    commands.registerCommand(cmdIds.TOGGLE_HIGHLIGHT_UNTYPED_COMMAND_ID, () =>
-      toggleUntypedCodeHighlighting(sorbetExtensionContext),
-    ),
-    commands.registerCommand(
-      cmdIds.CONFIGURE_HIGHLIGHT_UNTYPED_COMMAND_ID,
-      () => configureUntypedCodeHighlighting(sorbetExtensionContext),
-    ),
-    commands.registerCommand(
-      cmdIds.TOGGLE_TYPED_FALSE_COMPLETION_NUDGES_COMMAND_ID,
-      () => toggleTypedFalseCompletionNudges(sorbetExtensionContext),
-    ),
+    r(cmdIds.SORBET_SAVE_PACKAGE_FILES, () => savePackageFiles(extensionContext)),
+    r(cmdIds.TOGGLE_HIGHLIGHT_UNTYPED_COMMAND_ID, () => toggleUntypedCodeHighlighting(extensionContext)),
+    r(cmdIds.CONFIGURE_HIGHLIGHT_UNTYPED_COMMAND_ID, () => configureUntypedCodeHighlighting(extensionContext)),
+    r(cmdIds.TOGGLE_TYPED_FALSE_COMPLETION_NUDGES_COMMAND_ID, () => toggleTypedFalseCompletionNudges(extensionContext)),
   );
 
   // Start the extension.
-  await sorbetExtensionContext.statusProvider.startSorbet();
+  await extensionContext.statusProvider.startSorbet();
 
   // This exposes Sorbet Extension API.
-  const api = new SorbetExtensionApiImpl(sorbetExtensionContext);
+  const api = new SorbetExtensionApiImpl(extensionContext);
   context.subscriptions.push(api);
   return api.toApi();
 }
