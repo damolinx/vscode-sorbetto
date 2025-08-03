8e938d694e4e3095779d24bf31275b8981e72764
Daniel Molina <damolina@outlook.com>
2025-08-02 21:08:17 -0700

    Improve `gem` autocomplete. Support cancellation in gem request.

--- src/providers/gemfileCompletionProvider.ts
+++ src/providers/gemfileCompletionProvider.ts
@@ -1,12 +1,13 @@
 import * as vscode from 'vscode';
 import * as https from 'https';
 
-export const MINIMUM_HINT_LENGTH = 1;
+export const TRIGGER_CHARACTERS: readonly string[] = ['"', "'"];
 
 export function registerGemfileCompletionProvider(): vscode.Disposable {
   return vscode.languages.registerCompletionItemProvider(
-    { pattern: '**/Gemfile', scheme: 'file' },
+    { pattern: '**/Gemfile' },
     new GemfileCompletionProvider(),
+    ...TRIGGER_CHARACTERS
   );
 }
 
@@ -17,33 +18,40 @@ export class GemfileCompletionProvider implements vscode.CompletionItemProvider 
   async provideCompletionItems(
     document: vscode.TextDocument,
     position: vscode.Position,
-    _token: vscode.CancellationToken,
+    token: vscode.CancellationToken,
     _context: vscode.CompletionContext,
-  ): Promise<vscode.CompletionItem[] | undefined> {
-    const line = document.lineAt(position).text.substring(0, position.character + 1);
-    const groups = line.match(/^\s*gem\s*(?<startQuote>['"])(?<hint>[^"']+)(?<endQuote>\1?)$/)?.groups;
-    if (groups && groups.hint.length >= MINIMUM_HINT_LENGTH) {
-      const suggestions = await getGems(groups.hint);
-      return suggestions.map((gemName) => {
-        const item = new vscode.CompletionItem(gemName, vscode.CompletionItemKind.Reference);
-        if (!groups.endQuote) {
-          item.insertText = gemName + groups.startQuote;
-        }
-        return item;
-      });
+  ): Promise<vscode.CompletionList | undefined> {
+    const line = document.lineAt(position).text;
+    const match = line.match(/\bgem\s*(?<start>["'])(?<hint>[^"']*)(?<end>\k<start>)/d);
+    if (!match || !match.groups || !match.indices?.groups) {
+      return;
     }
 
-    return;
+    // Ensure cursor is inside the quotes
+    const { start: [openStart], end: [_, endClose] } = match.indices.groups;
+    if (position.character <= openStart || position.character >= endClose) {
+      return;
+    }
+
+    const gems = await getGems(match.groups.hint || 'a');
+    return new vscode.CompletionList(gems.map(gem => {
+      const item = new vscode.CompletionItem(gem, vscode.CompletionItemKind.Reference);
+      item.insertText = gem;
+      return item;
+    }), true);
 
     // TODO: read the `source` and map to different sources if possible.
-    async function getGems(hint: string): Promise<string[]> {
+    function getGems(hint: string): Promise<string[]> {
       return new Promise((resolve) => {
-        https.get(`https://rubygems.org/api/v1/search/autocomplete?query=${encodeURIComponent(hint)}`, (res) => {
+        const request = https.get(`https://rubygems.org/api/v1/search/autocomplete?query=${encodeURIComponent(hint)}`, (res) => {
           let data = '';
           res.on('data', (chunk) => data += chunk)
             .on('end', () => resolve(data.slice(1, -1).replace(/"/g, '').split(',')))
             .on('error', (_err) => resolve([]));
         });
+        token.onCancellationRequested(() =>
+          request.destroy(new Error('Request canceled'))
+        );
       });
     }
   }
