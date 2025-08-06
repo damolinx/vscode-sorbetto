6355c1ab2049e0176c372f6b511e91b6ac670094
Daniel Molina <damolina@outlook.com>
2025-08-05 20:37:38 -0700

    Add message

--- src/commands/utils.ts
+++ src/commands/utils.ts
@@ -1,16 +1,18 @@
 import { TerminalOptions, Uri, window } from 'vscode';
 
 export async function executeCommandsInTerminal(options: { commands: string[], name?: string, cwd?: string | Uri, preserveFocus?: boolean }) {
+  const cmd = options.commands.join('&&');
   const terminalOptions: TerminalOptions = {
     cwd: options.cwd,
+    message: `\x1b[1mRunning\x1b[0m: ${cmd}`,
     name: options.name,
   };
   if (process.platform === 'win32') {
     terminalOptions.shellPath = 'cmd.exe';
-    terminalOptions.shellArgs = ['/K', `${options.commands.join('&&')} && pause`];
+    terminalOptions.shellArgs = ['/K', `${cmd} && pause`];
   } else {
     terminalOptions.shellPath = '/bin/bash';
-    terminalOptions.shellArgs = ['-c', `${options.commands.join('&&')}; read -n1 -rsp "Press any key to continue ..."`];
+    terminalOptions.shellArgs = ['-c', `${cmd}; read -n1 -rsp "Press any key to continue ..."`];
   }
 
   const terminal = window.createTerminal(terminalOptions);
