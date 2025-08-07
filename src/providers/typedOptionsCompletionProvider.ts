9ede94b057f48955570281ca2647f920b1b605ab
Daniel Molina <damolina@outlook.com>
2025-08-06 20:24:29 -0700

    Improve `#typed` completion

--- src/providers/typedOptionsCompletionProvider.ts
+++ src/providers/typedOptionsCompletionProvider.ts
@@ -18,26 +18,24 @@ export class TypedOptionsCompletionProvider implements vscode.CompletionItemProv
     document: vscode.TextDocument,
     position: vscode.Position,
     _token: vscode.CancellationToken,
-    context: vscode.CompletionContext,
-  ): Promise<vscode.CompletionItem[] | undefined> {
+    _context: vscode.CompletionContext,
+  ): Promise<vscode.CompletionList | undefined> {
     const line = document.lineAt(position).text.substring(0, position.character);
-
-    if (TRIGGER_CHARACTERS.some((c) => c === context.triggerCharacter) || context.triggerKind == vscode.CompletionTriggerKind.Invoke) {
-      if (/^\s*#\s*typed:/.test(line)) {
-        return ([
-          ['ignore', new vscode.MarkdownString('Sorbet completely ignores file.')],
-          ['false', new vscode.MarkdownString('Sorbet only reports errors related to syntax, constant resolution and `sig`s.')],
-          ['true', new vscode.MarkdownString('Sorbet reports type error in addition to `false`-level rules.')],
-          ['strict', new vscode.MarkdownString('Sorbet requires all methods to have `sig`s in addition to `true`-level rules.')],
-          ['strong', new vscode.MarkdownString('Sorbet no longer allows `T.untyped`  in addition to `strict`-level rules.')],
-        ] as [string, vscode.MarkdownString][]).map(([name, documentation]) => {
-          const item = new vscode.CompletionItem(name, vscode.CompletionItemKind.Snippet);
-          item.documentation = documentation;
-          return item;
-        });
-      }
+    if (!/^\s*#\s*typed:\s*[a-z]*$/.test(line)) {
+      return;
     }
 
-    return;
+    return new vscode.CompletionList(
+      ([
+        ['ignore', new vscode.MarkdownString('Sorbet completely ignores file.')],
+        ['false', new vscode.MarkdownString('Sorbet only reports errors related to syntax, constant resolution and `sig`s.')],
+        ['true', new vscode.MarkdownString('Sorbet reports type error in addition to `false`-level rules.')],
+        ['strict', new vscode.MarkdownString('Sorbet requires all methods to have `sig`s in addition to `true`-level rules.')],
+        ['strong', new vscode.MarkdownString('Sorbet no longer allows `T.untyped` in addition to `strict`-level rules.')],
+      ] as [string, vscode.MarkdownString][]).map(([name, documentation]) => {
+        const item = new vscode.CompletionItem(name, vscode.CompletionItemKind.Snippet);
+        item.documentation = documentation;
+        return item;
+      }));
   }
 }
