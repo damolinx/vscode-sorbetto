e1fe0ff727863fde304462e143da6412f09f3998
Daniel Molina <damolina@outlook.com>
2025-08-15 09:14:34 -0700

    Update linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@
  */
 // @ts-check
 import js from '@eslint/js';
+import eslintImport from 'eslint-plugin-import';
 import tseslint from 'typescript-eslint';
 import stylistic from '@stylistic/eslint-plugin';
 
@@ -13,20 +14,44 @@ export default tseslint.config(
     ignores: [
       '.vscode-test',
       'out',
-    ]
+    ],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   ...tseslint.configs.stylistic,
   {
     plugins: {
-      '@stylistic': stylistic
+      '@stylistic': stylistic,
+      '@eslint/import': eslintImport,
+
     },
     rules: {
       'arrow-parens': ['error', 'always'],
       'curly': 'warn',
       'no-trailing-spaces': 'error',
-      '@stylistic/comma-dangle': ["error", "always-multiline"],
+      '@eslint/import/order': [
+        'error',
+        {
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'never',
+          pathGroups: [
+            {
+              pattern: 'vscode',
+              group: 'builtin',
+              position: 'before',
+            },
+            {
+              pattern: 'vscode-languageclient/**',
+              group: 'builtin',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+        },
+      ],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/indent': ['error', 2],
       '@stylistic/quotes': ['error', 'single'],
       '@stylistic/semi': ['error', 'always'],
       '@typescript-eslint/naming-convention': [
@@ -37,7 +62,7 @@ export default tseslint.config(
         }
       ],
       '@typescript-eslint/no-empty-function': 'off',
-      '@typescript-eslint/no-explicit-any' : 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
