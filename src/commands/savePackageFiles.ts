49f9321aeb11bb709676c161391cf543bf3f153c
Daniel Molina <damolina@outlook.com>
2026-01-15 20:05:39 -0800

    Clean-up API.

--- src/commands/savePackageFiles.ts
+++ src/commands/savePackageFiles.ts
@@ -5,27 +5,19 @@ import { ExtensionContext } from '../extensionContext';
 export const PACKAGE_FILENAME = '__package.rb';
 
 /**
- * Save all __package.rb files with changes.
- *
- * @param context Sorbet extension context.
- * @return `true` if all the files were successfully saved.
+ * Save all modified __package.rb files.
+ * @param context Extension context.
  */
-export async function savePackageFiles(context: ExtensionContext): Promise<boolean> {
-  const pkgDocs = vscode.workspace.textDocuments.filter(
+export async function savePackageFiles(context: ExtensionContext): Promise<void> {
+  const documents = vscode.workspace.textDocuments.filter(
     (document) => document.isDirty && basename(document.fileName) === PACKAGE_FILENAME,
   );
 
-  switch (pkgDocs.length) {
-    case 0:
-      context.log.trace('SavePackageFiles: nothing to save');
-      return true;
-    case 1:
-      context.log.trace('SavePackageFiles: saving package file');
-      return await pkgDocs[0].save();
-    default:
-      context.log.trace('SavePackageFiles: Saving package files', pkgDocs.length);
-      return (await Promise.all(pkgDocs.map((document) => document.save()))).every(
-        (saved) => saved,
-      );
+  if (documents.length === 0) {
+    context.log.trace('SavePackageFiles: nothing to save');
+    return;
   }
+
+  context.log.trace('SavePackageFiles: Saving package files', documents.length);
+  await Promise.allSettled(documents.map((document) => document.save()));
 }
